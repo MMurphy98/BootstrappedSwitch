d71838aacf18a0644ccea71374f5f8b1105bf95a
MMurphy98 <mjg654654@outlook.com>
2022-05-23 11:55:19 +0800

    Format the PhaseOrder.m

--- Model/PhaseOrder.m
+++ Model/PhaseOrder.m
@@ -1,10 +1,15 @@
 function T = PhaseOrder(x, Fi, Fs)
-%use phase order to rearrange the data
-%   
+%use phase order to rearrange the data;
+%   x:  input data;
+%   Fi: input frequency;
+%   Fs: sample frequency;  
+
     step = Fi / Fs;
     N = length(x);
     index = linspace(1,N,N);
 
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    % transpose the martix
     if (isrow(x))
         x = x';
     end
@@ -12,14 +17,14 @@ function T = PhaseOrder(x, Fi, Fs)
     if (isrow(index))
         index = index';
     end
-
-    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+    
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    % create the table for output
     T = array2table([index, x], "VariableNames",{'Index', 'Value'});
     
     T.PhaseOrder = mod((T.Index * step), 1);
     
+    % sorted the table by PhaseOrder
     T = sortrows(T, "PhaseOrder");
-
-
+    
 end
