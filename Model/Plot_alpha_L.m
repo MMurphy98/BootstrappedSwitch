c73d5d2d3357f81e8b16f4cc0f0dd401bedf6f81
MMurphy98 <mjg654654@outlook.com>
2022-05-26 11:43:38 +0800

    Update Plot_alpha_L.m
    Update alpha

--- Model/Plot_alpha_L.m
+++ Model/Plot_alpha_L.m
@@ -1,6 +1,6 @@
 %% Get date 
 L = [0.2, 0.4, 0.6,0.8, 1, 2, 3];
-alpha = [-0.85, -0.1, 0.19, 0.3, 0.4, 0.6, 0.7];
+alpha = [-1.15, -0.15, 0.15, 0.3, 0.4, 0.6, 0.68];
 
 %% Plot the relationship between L and alpha
 figure
