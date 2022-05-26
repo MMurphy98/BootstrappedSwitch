45364b29fb36298a68b5ca32b4fb9efae3647e9b
MMurphy98 <mjg654654@outlook.com>
2022-05-26 11:29:49 +0800

    Update SD_Exchange.m
    完成系数矩阵确定

--- Model/SD_Exchange.m
+++ Model/SD_Exchange.m
@@ -5,12 +5,12 @@ function HD3 = SD_Exchange(Ron, V0, C1, Fin, alpha)
     VTH = 0.457;
 
     win = 2*pi .* Fin;
-    P = V0*C1/(VDD-VTH)*(alpha-1) .* Ron .*win;
-    Q = V0*C1/(VDD-VTH)*(alpha) .* Ron .*win;
-%     K1 = V0*C1/(VDD-VTH)*(alpha - 0.5) .*Ron .*win
-%     K2 = V0*C1/(VDD-VTH)*(0.5) .*Ron .*win
-%     P = K1 - K2;
-%     Q = K2 + K2;
+%     P = V0*C1/(VDD-VTH)*(alpha-1) .* Ron .*win
+%     Q = V0*C1/(VDD-VTH)*(alpha) .* Ron .*win
+    K1 = V0*C1/(VDD-VTH)*(alpha - 0.5) .*Ron .*win;
+    K2 = V0*C1/(VDD-VTH)*(0.5) .*Ron .*win;
+    P = K1 - K2;
+    Q = K1 + K2;
     
     R2_num = 30*pi*(-16.*P.^2 + 48*pi.*P + 9*pi^2.*Q.^2) .* Ron;
     R2_den = -512.*P.^3 + 2688*pi.*P.^2 + 2160*pi^3 + ...
