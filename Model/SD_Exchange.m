875fd276cc23eb088ebc12342eb835e2f4c93628
Mmurphy98 <mjg654654@outlook.com>
2022-05-26 10:28:55 +0800

    Update SD_Exchange.m

--- Model/SD_Exchange.m
+++ Model/SD_Exchange.m
@@ -4,6 +4,17 @@ function HD3 = SD_Exchange(Ron, V0, C1, Fin, alpha)
     VDD = 1.8;
     VTH = 0.457;
 
-    P = 2*pi*V0*C1/(VDD-VTH)*(alpha-1) .* Ron .*Fin;
-    Q = 2*pi*V0*C1/(VDD-VTH)*(alpha) .* Ron .*Fin;
+%     P = 2*pi*V0*C1/(VDD-VTH)*(alpha-1) .* Ron .*Fin;
+%     Q = 2*pi*V0*C1/(VDD-VTH)*(alpha) .* Ron .*Fin;
+
+    P = 2*pi*V0*C1/(VDD-VTH)*(1-alpha) .* Ron .*Fin;
+    Q = -2*pi*V0*C1/(VDD-VTH)*(alpha) .* Ron .*Fin;
+    
+    R2_num = 30*pi*(-16.*P.^2 + 48*pi.*P + 9*pi^2.*Q.^2) .* Ron;
+    R2_den = -512.*P.^3 + 2688*pi.*P.^2 + 2160*pi^3 + ...
+        9*pi^2.*P.*(-464+5.*Q.^2);
+    R2 = R2_num ./ R2_den;
+    
+    HD3 = pi*Fin * C1 .* R2;
+    HD3 = mag2db(HD3);
 end
