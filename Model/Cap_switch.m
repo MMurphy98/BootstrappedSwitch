dc1d73ed7a7ae92815084e7b864af26a7c2a6e14
MMurphy98 <mjg654654@outlook.com>
2022-06-08 22:19:40 +0800

    Create Cap_Switch.mlx

--- Model/Cap_switch.m
+++ Model/Cap_switch.m
@@ -5,19 +5,18 @@ win = 2*pi *Fin;
 C1 = 38.72E-12;
 Vin = 0.895;
 Vcm = 0.9;
-Cb = 10E-12;
+Cb = 5E-12;
 Cp = 1E-12;
 Ctot = Cb + 3*Cp;
 k1 = cos(win*ts)
 k2 = sin(win*ts)
 Ck = Cp*sqrt((2+k1)^2 +k2^2);
-
+VTH = 0.457;
 V1 = Ck/(Ctot)*Vin
 V0 = Cb/Ctot*1.8 - 3*Cp/Ctot*Vcm
 
-Ron0 = 213 / 32;
-Ron = Ron0 * 1.8 / V0
+Ron = 1/(280E-6 * 32 * 3 / 0.2) / (V0 - VTH)
 
-HD2 = mag2db(1/2*C1*win*Ron*(V1/V0))
+HD2 = mag2db(1/4*C1*win*Ron*(V1/V0))
 
-HD3 = mag2db(1/2*C1*win*Ron*(V1/V0)^2)
+HD3 = mag2db(1/4*C1*win*Ron*(V1/V0)^2)
